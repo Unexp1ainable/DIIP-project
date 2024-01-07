1e1402aeb29f3c05d27096c82924dde22ed8c4f1
Matej Kunda <36matej36@gmail.com>
2024-01-07 14:13:42 +0100

    Added image calibration

--- coinDetect.m
+++ coinDetect.m
@@ -8,7 +8,13 @@ imageFiles = dir(fullfile(path, '*.JPG'));
 for i = 1:numel(imageFiles)
     img = imread(fullfile(path, imageFiles(i).name));
 
-    out = preprocess2(img);
+    % Calibrate image
+    bias = imread(fullfile(strcat(path, '/bias'), 'bias.JPG'));
+    dark = imread(fullfile(strcat(path, '/dark'), 'dark.JPG'));
+    flat = imread(fullfile(strcat(path, '/flat'), 'flat.JPG'));
+    img = calibrate(img, bias, dark, flat, true);
+
+    out = preprocess2(img, false);
 
     figure; imshow(out); title(imageFiles(i).name);
 
@@ -21,6 +27,20 @@ for i = 1:numel(imageFiles)
     title(imageFiles(i).name);
     hold off;
 end
+
+function out = calibrate(img, bias, dark, flat, show)
+    norm_F = flat - bias - dark;
+    norm_F = uint8(double(norm_F)./double(max(norm_F(:))));
+
+    calibrated_image = (img - bias - dark) ./ norm_F;
+    out = calibrated_image;
+
+    if show
+        subplot(121); imshow(img); title('Raw Image')
+        subplot(122);imshow(calibrated_image); title('Calibrated Image')
+    end
+end
+
 
 function out = preprocess1(img)
     grayImage = rgb2gray(img);
@@ -37,13 +57,16 @@ function out = preprocess1(img)
     out = imfill(cleanedImg, 'holes');
 end
 
-function out = preprocess2(img)
+function out = preprocess2(img, show)
     grayImage = rgb2gray(img);
 
     smoothedImage = imgaussfilt(grayImage, 2); %2
 
     edgeImage = edge(smoothedImage, 'canny', [0.05 0.2]); %0.05 0.2
-    %figure; imshow(edgeImage);
+
+    if show
+        figure; imshow(edgeImage);
+    end
 
     se = strel('disk', 10);
     dilatedImage = imclose(edgeImage, se);
